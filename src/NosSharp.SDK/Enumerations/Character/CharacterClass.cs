98854591f0702117d43f248187f4f8c0e574d6d6
Blowa <blowa@nossharp.com>
2018-02-11 10:50:43 +0100

    add folders & character HpAlgorithms

---
+++ src/NosSharp.SDK/Enumerations/Character/CharacterClass.cs
@@ -0,0 +1,13 @@
+﻿namespace NosSharp.SDK.Enumerations.Character
+{
+    /// <summary>
+    /// CharacterClass enum
+    /// </summary>
+    public enum CharacterClass
+    {
+        Adventurer = 0x00,
+        Swordman = 0x01,
+        Archer = 0x02,
+        Magician = 0x03
+    }
+}
---
+++ src/NosSharp.SDK/Enumerations/Character/CharacterGender.cs
@@ -0,0 +1,11 @@
+﻿namespace NosSharp.SDK.Enumerations.Character
+{
+    /// <summary>
+    /// Character Gender based on Nostale's packets
+    /// </summary>
+    public enum CharacterGender
+    {
+        Male = 0x00,
+        Female = 0x01
+    }
+}
