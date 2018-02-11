a8ed7c2e7aab9926529b8f7416ebef04df4b3be3
Blowa <blowa@nossharp.com>
2018-02-11 10:34:16 +0100

    add SDK basis

---
+++ src/NosSharp.SDK/Entities/Character.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NosSharp.ECS.Components;
+using NosSharp.ECS.Entities;
+
+namespace NosSharp.SDK.Entities
+{
+    public class Character : IEntity
+    {
+        private readonly Dictionary<Type, IComponent> _components = new Dictionary<Type, IComponent>();
+
+        public long Id { get; }
+        public Type EntityType => typeof(Character);
+
+        public bool HasComponent<T>()
+        {
+            return HasComponent(typeof(T));
+        }
+
+        public bool HasComponent(Type type)
+        {
+            return _components.ContainsKey(type);
+        }
+
+        public T GetComponent<T>() where T : IComponent
+        {
+            return (T) GetComponent(typeof(T));
+        }
+
+        public IComponent GetComponent(Type type)
+        {
+            return !_components.TryGetValue(type, out IComponent component) ? null : component;
+        }
+
+        public IComponent[] GetComponents()
+        {
+            return _components.Values.ToArray();
+        }
+
+        public IComponent[] GetComponents<T>() where T : IComponent
+        {
+            return GetComponents(typeof(T));
+        }
+
+        /// <inheritdoc />
+        public IComponent[] GetComponents(Type type)
+        {
+            return _components.Values.Where(s => s.Type == type).ToArray();
+        }
+
+        public void AddComponent(IComponent component, Type type)
+        {
+            _components.TryAdd(type, component);
+        }
+
+        public void AddComponent<T>(IComponent component) where T : class
+        {
+            AddComponent(component, typeof(T));
+        }
+
+        public bool RemoveComponent<T>()
+        {
+            return RemoveComponent(typeof(T));
+        }
+
+        public bool RemoveComponent(Type type)
+        {
+            return _components.Remove(type);
+        }
+
+        public void Destroy()
+        {
+        }
+    }
+}
