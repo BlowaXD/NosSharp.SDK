a85654f9050e87c7e2432728e53ee1c3f40f8f5e
Blowa <blowaxd693@gmail.com>
2018-03-03 19:56:46 +0100

    finish ItemDTO

---
+++ src/NosSharp.SDK.Packets/SentByServer/WreqPacket.cs
@@ -0,0 +1,18 @@
+﻿using NosSharp.SDK.Packets.Serialization;
+
+namespace NosSharp.SDK.Packets.SentByServer
+{
+    [PacketHeader("wreq")]
+    public class WreqPacket : PacketBase
+    {
+        #region Properties
+
+        [PacketIndex(1)]
+        public long? Param { get; set; }
+
+        [PacketIndex(0)]
+        public byte Value { get; set; }
+
+        #endregion
+    }
+}
